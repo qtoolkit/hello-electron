db591ba7a3c041e9bdf890c69923113af6c5d3e1
jim <xianjimli@hotmail.com>
2017-01-08 13:26:27 +0800

    update build script

--- release.bat
+++ release.bat
@@ -2,11 +2,9 @@ rmdir /S /Q release
 mkdir release\node_modules
 mkdir release\build\Release
 
-copy /Y electron-package.json release\package.json
+copy /Y package.json release\package.json
 copy /Y build\Release\*.node release\build\Release
 copy /Y  index.html release
-xcopy /Y  www release\www\
+xcopy /Y /S  www release\www\
 xcopy /Y  node_modules\bindings release\node_modules\bindings\
 
-node_modules\.bin\asar pack release app.asar
-node_modules\.bin\asar list app.asar
--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ rm -rf release electron
 mkdir -p release/node_modules
 mkdir -p release/build/Release/
 
-cp -fv electron-package.json release/package.json
+cp -fv package.json release/package.json
 cp -fv build/Release/*.node release/build/Release
 cp -frv node_modules/bindings release/node_modules/bindings
 
@@ -10,9 +10,5 @@ for f in index.html	www
 do
 	cp -rfv $f release
 done
-
-./node_modules/.bin/asar  pack release app.asar
-./node_modules/.bin/asar list app.asar
-
 
 
